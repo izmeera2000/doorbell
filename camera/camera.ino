8cdf29e21351f81c8b4c792ea976a0f623788c62
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-26 01:45:26 +0800

    sad
    assadasdsa

--- camera/camera.ino
+++ camera/camera.ino
@@ -36,8 +36,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "**********";
-const char *password = "**********";
+const char* ssid = "iPhone";          // Replace with your Wi-Fi SSID
+const char* password = "Alamak323";   // Replace with your Wi-Fi Password
 
 void startCameraServer();
 void setupLedFlash(int pin);
